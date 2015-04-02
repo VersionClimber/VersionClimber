218f1ac16b3a67dbdd8bc79c443c3f23fb6c4fb6
Christophe Pradal <christophe.pradal@inria.fr>
2015-04-02 14:25:51 +0000

    Initial version
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vflexql/trunk@2 4b932556-c637-48da-96c4-f11f38fc8080

---
+++ doc/user/index.rst
@@ -0,0 +1,24 @@
+.. _vflexql_user:
+
+VFlexQL User Guide
+#######################################
+
+:Version: |version|
+:Release: |release|
+:Date: |today|
+
+This manual details functions, modules, and objects included in 
+VFlexQL, describing what they are and what they do. For a complete 
+reference guide, see :ref:`vflexql_reference`.
+
+.. warning::
+
+   This "User Guide" is still very much in progress. 
+   Many aspects of VFlexQL are not covered.
+
+   More documentation can be found on the
+   `openalea <http://openalea.gforge.inria.fr>`__ wiki.
+
+.. toctree::
+
+    manual.rst
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id: $"
+
+from setuptools import setup, find_packages
+
+# Packages list, namespace and root directory of packages
+
+packages = find_packages('.')
+
+# dependencies to other eggs
+setup_requires = []
+install_requires = []
+
+# web sites where to find eggs
+dependency_links = ['http://openalea.gforge.inria.fr/pi']
+
+name = 'VFlexQL'
+description = 'VFlexQL : Version Flexible Query Language'
+long_description = '''
+
+'''
+authors = 'christophe.pradal@inria.fr shasha@courant.nyu.edu Sarah.Cohen_Boulakia@lri.fr Patrick.Valduriez@inria.fr'.split()
+authors_email = 'christophe.pradal@inria.fr'
+url = 'http://openalea.gforge.inria.fr'
+license = 'Cecill-C'
+
+setup(
+    name=name,
+    version='1.0.0',
+    description=description,
+    long_description=long_description,
+    author=authors,
+    author_email=authors_email,
+    url=url,
+    license=license,
+    keywords='',
+
+    # package installation
+    packages=packages,
+
+    zip_safe=False,
+
+    # Dependencies
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    )
