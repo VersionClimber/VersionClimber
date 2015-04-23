895724058aaffd9462abebbb6dcdacfbc40c4daf
Christophe Pradal <christophe.pradal@inria.fr>
2015-04-23 16:10:04 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vflexql/trunk@8 4b932556-c637-48da-96c4-f11f38fc8080

--- test/test_climber.py
+++ test/test_climber.py
@@ -13,24 +13,24 @@ liquid.activate()
 #print results
 
 compatibilities= []
-compatibilities.append([1, 11, 11, 2, 21, 21])
-compatibilities.append([1, 12, 12, 2, 22, 22])
-compatibilities.append([1, 15, 15, 2, 28, 28])
-compatibilities.append([1, 11, 11, 3, 31, 31])
-compatibilities.append([1, 12, 12, 3, 32, 32])
-compatibilities.append([1, 15, 15, 3, 38, 38])
-compatibilities.append([1, 11, 11, 4, 41, 41])
-compatibilities.append([1, 12, 12, 4, 42, 42])
-compatibilities.append([1, 15, 15, 4, 48, 48])
-compatibilities.append([2, 21, 21, 3, 31, 31])
-compatibilities.append([2, 22, 22, 3, 32, 32])
-compatibilities.append([2, 28, 28, 3, 38, 38])
-compatibilities.append([2, 21, 21, 4, 41, 41])
-compatibilities.append([2, 22, 22, 4, 42, 42])
-compatibilities.append([2, 28, 28, 4, 48, 48])
-compatibilities.append([3, 31, 31, 4, 41, 41])
-compatibilities.append([3, 32, 32, 4, 42, 42])
-compatibilities.append([3, 38, 38, 4, 48, 48])
+# compatibilities.append([1, 11, 11, 2, 21, 21])
+# compatibilities.append([1, 12, 12, 2, 22, 22])
+# compatibilities.append([1, 15, 15, 2, 28, 28])
+# compatibilities.append([1, 11, 11, 3, 31, 31])
+# compatibilities.append([1, 12, 12, 3, 32, 32])
+# compatibilities.append([1, 15, 15, 3, 38, 38])
+# compatibilities.append([1, 11, 11, 4, 41, 41])
+# compatibilities.append([1, 12, 12, 4, 42, 42])
+# compatibilities.append([1, 15, 15, 4, 48, 48])
+# compatibilities.append([2, 21, 21, 3, 31, 31])
+# compatibilities.append([2, 22, 22, 3, 32, 32])
+# compatibilities.append([2, 28, 28, 3, 38, 38])
+# compatibilities.append([2, 21, 21, 4, 41, 41])
+# compatibilities.append([2, 22, 22, 4, 42, 42])
+# compatibilities.append([2, 28, 28, 4, 48, 48])
+# compatibilities.append([3, 31, 31, 4, 41, 41])
+# compatibilities.append([3, 32, 32, 4, 42, 42])
+# compatibilities.append([3, 38, 38, 4, 48, 48])
 
 orderofpackages = [1, 3, 4, 2, 3, 4, 3, 1, 2]
 
@@ -42,7 +42,7 @@ sourcemap = { 1: [11, 12, 13, 14, 15, 16, 17, 18, 19],
 default = {1:11,2:21, 3:31, 4:41} # configuration that works
 
 constraints = {}
-todolist = [3,1]
+todolist = [3,2,4,1]
 
 # set global variables
 liquidparser.compatibilities = compatibilities
