9bad1293c9f92508fc579c824b4674335e835739
pradal <christophe.pradal@inria.fr>
2016-06-01 14:00:06 +0200

    Display the result on the console

--- example/tuto01/tuto2.py
+++ example/tuto01/tuto2.py
@@ -15,3 +15,10 @@ try:
     print liquidparser.memory
 finally:
     env.restore()
+
+res = []
+for k, v in endconfig.iteritems():
+    pkg = env.int2pkg[k]
+    res.append('(%s,%s)'%(env.int2pkg[k], env.commits[env.int2pkg[k]][v-1]))
+
+print('Solution is: \n%s\n'%('\n'.join(res)))
