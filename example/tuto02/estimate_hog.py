8693e6b0e82f5608f49f2490c5d1fa354c5b425f
pradal <christophe.pradal@inria.fr>
2016-06-01 17:32:00 +0200

    Close #27 issue: Change the test to estimate HOG based on images database

--- example/tuto02/estimate_hog.py
+++ example/tuto02/estimate_hog.py
@@ -1,12 +1,28 @@
-import sys, traceback
-try:
-    import foo
-    from hoo import hoo
+# Import the modules
+from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+import numpy as np
+from collections import Counter
 
-    hoo()
-except Exception, e:
-    f = open('error.txt','w')
-    traceback.print_exc(file=f)
-    f.close()
-    raise e
+# Load the dataset
+dataset = datasets.fetch_mldata("MNIST Original")
 
+# Extract the features and labels
+features = np.array(dataset.data, 'int16') 
+labels = np.array(dataset.target, 'int')
+
+# Extract the hog features
+list_hog_fd = []
+for feature in features:
+    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    list_hog_fd.append(fd)
+hog_features = np.array(list_hog_fd, 'float64')
+
+print "Count of digits in dataset", Counter(labels)
+
+# Create an linear SVM object
+clf = LinearSVC()
+
+# Perform the training
+clf.fit(hog_features, labels)
