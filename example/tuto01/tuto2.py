a9873ded3eb073bc657122a57894d46b79f7fe90
revesansparole <revesansparole@gmail.com>
2016-06-01 14:05:30 +0200

    cleaned a bit tuto2

--- example/tuto01/tuto2.py
+++ example/tuto01/tuto2.py
@@ -1,9 +1,4 @@
-import git
-import versionclimber
-from versionclimber import liquid, config
-from versionclimber import liquidparser
-###############################################################################
-# Load config
+from versionclimber import liquid, liquidparser
 
 config_file = 'config.yaml'
 env = liquid.YAMLEnv(config_file)
@@ -12,13 +7,12 @@ constraints, todolist = env.monkey_patch(liquidparser)
 
 try:
     endconfig = liquidparser.liquidclimber(constraints, todolist)
-    print liquidparser.memory
 finally:
     env.restore()
 
 res = []
 for k, v in endconfig.iteritems():
     pkg = env.int2pkg[k]
-    res.append('(%s,%s)'%(env.int2pkg[k], env.commits[env.int2pkg[k]][v-1]))
+    res.append('(%s,%s)' % (env.int2pkg[k], env.commits[env.int2pkg[k]][v-1]))
 
-print('Solution is: \n%s\n'%('\n'.join(res)))
+print('Solution is: \n%s\n' % ('\n'.join(res)))
