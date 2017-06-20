7292c98b10436d639f6e609c58cf60d953841d81
pradal <christophe.pradal@inria.fr>
2017-06-20 10:41:51 +0200

    Add test

--- test/test_pypi.py
+++ test/test_pypi.py
@@ -6,8 +6,11 @@ from versionclimber import utils
 def test_sklearn():
     versions = utils.pypi_versions('scikit-learn')
     assert versions[0] == '0.9'
+    assert len(versions) >= 24
+
 
 
 def test_skimage():
     versions = utils.pypi_versions('scikit-image')
     assert versions[0] == '0.7.2'
+    assert len(versions) >= 18
