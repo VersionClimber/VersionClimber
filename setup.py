5773f6ddf5ee7be9819202c5f84a029921408c51
Christophe Pradal <christophe.pradal@inria.fr>
2017-12-22 10:34:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ license = 'Cecill-C'
 
 setup(
     name=name,
-    version='1.1.0',
+    version='1.2.0',
     description=description,
     long_description=long_description,
     author=authors,
