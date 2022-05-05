60ee8efe769f3e5c307a87358b678b7f8c21915e
pradal <christophe.pradal@inria.fr>
2022-05-05 12:11:28 +0200

    Update test_conda

--- test/test_conda.py
+++ test/test_conda.py
@@ -5,24 +5,24 @@ from versionclimber import utils
 
 
 def test_sklearn():
-    versions = utils.conda_versions('scikit-learn')
-    assert versions[0] == '0.11'
-    assert len(versions) >= 16
+    versions = utils.conda_versions('scikit-learn', build='')
+    assert versions[0] == '0.17.1'
+    assert len(versions) >= 20
 
 
 def test_skimage():
-    versions = utils.conda_versions('scikit-image')
-    assert versions[0] == '0.7.2'
-    assert len(versions) >= 12
+    versions = utils.conda_versions('scikit-image', , build='')
+    assert versions[0] == '0.12.3'
+    assert len(versions) >= 19
 
 def test_mtg_channels():
-    versions = utils.conda_versions('openalea.mtg', channels=['openalea'])
-    assert '2.0.1' in versions
+    versions = utils.conda_versions('openalea.mtg', channels=['openalea3'], , build='')
+    assert '2.1.1' in versions
     assert len(versions) >= 1
 
 def test_nonpy():
-    versions_default = utils.conda_versions('gmp')
-    versions = utils.conda_versions('gmp', channels=['openalea'])
+    versions_default = utils.conda_versions('gmp', build='')
+    versions = utils.conda_versions('gmp', channels=['openalea'], build='')
     if len(versions_default) < len(versions):
         assert len(versions_default) <= len(versions), str(len(versions_default)) + ', ' + str(len(versions))
         assert set(versions) - set(versions_default)
--- test/wip/simple/goo/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='goo',
-      version='1.0',
-      py_modules=['goo'],
-      )
