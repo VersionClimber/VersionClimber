50b3496fbaa18143981833db527a219cbc010483
pradal <christophe.pradal@inria.fr>
2022-05-05 10:20:03 +0200

    Update setup.py and remove utf-8

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-__revision__ = "$Id: $"
-
 from setuptools import setup
 
 # Packages list, namespace and root directory of packages
@@ -16,13 +13,15 @@ description = 'Version Climber: System and Algorithms for Package evolution in D
 long_description = description
 
 authors = 'Christophe Pradal, Sarah Cohen-Boulakia, Patrick Valduriez, Dennis Shasha'
-authors_email = 'christophe.pradal@inria.fr shasha@courant.nyu.edu Sarah.Cohen_Boulakia@lri.fr Patrick.Valduriez@inria.fr'.split()
+authors_email = 'christophe.pradal@cirad.fr, shasha@courant.nyu.edu, Sarah.Cohen_Boulakia@lri.fr, Patrick.Valduriez@inria.fr'
 url = 'https://github.com/VersionClimber/VersionClimber'
 license = 'Cecill-C'
 
+version='1.3.3'
+
 setup(
     name=name,
-    version='1.3.3',
+    version=version,
     description=description,
     long_description=long_description,
     author=authors,
